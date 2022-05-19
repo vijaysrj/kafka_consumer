2c9b2152add1907ebc175d57f274bed2e87ae914
vijay <vijay.srj@gmail.com>
2022-05-19 15:37:56 +0530

    changes

---
+++ src/main/java/com/example/kafka/consumer/KafkaConsumer.java
@@ -0,0 +1,19 @@
+package com.example.kafka.consumer;
+
+import java.util.Map;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+	
+
+	@KafkaListener(topics = "my_topic")
+	public void receive(String message) {
+		
+		
+		System.out.println(message);
+	}
+}
